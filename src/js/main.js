89d4ccb28520e4c3ec987a0495ff8f5e56de54c5
LeandroDevLab <prof.oliota@gmail.com>
2026-02-18 12:26:36 -0300

    :sparkles: feat: render supplements dynamically using swiper and forEach

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 /* Galeria Swiper */
 const swiper = new Swiper(".swiper", {
+  slidesPerView: 1.3, //quantos slides por vez cabem na tela
+
+  centeredSlides: true, //centralizando slide atual
+
   loop: true,
 
   pagination: {
@@ -24,10 +28,10 @@ const swiper = new Swiper(".swiper", {
 
   breakpoints: {
     768: {
-      slidesPerView: 2,
+      slidesPerView: 2.2,
     },
     1024: {
-      slidesPerView: 3,
+      slidesPerView: 3.5,
     },
   },
 });
@@ -74,3 +78,70 @@ function focusWeekDay() {
   });
 }
 focusWeekDay();
+
+/* ============================================================================= */
+/* ================= Preenchimento dinâmico dos suplementos ==================== */
+/* ============================================================================= */
+const suplementos = [
+  {
+    title: "Creatina Growth 250g",
+    subtitle: "com menor preço",
+    lastPrice: 120,
+    newPrice: 80,
+    imgSrc: "creatina.png",
+  },
+  {
+    title: "Whey Protein 1000g",
+    subtitle: "com menor preço",
+    lastPrice: 150,
+    newPrice: 110,
+    imgSrc: "whey.png",
+  },
+  {
+    title: "Growth Protein Bar 65g",
+    subtitle: "lanche ideal para você",
+    lastPrice: 19,
+    newPrice: 9.9,
+    imgSrc: "whey-barra.png",
+  },
+  {
+    title: "Growth Whey Protein 30g",
+    subtitle: "Dose de whey, prove os sabores",
+    lastPrice: 12,
+    newPrice: 9.9,
+    imgSrc: "whey-dose.png",
+  },
+];
+
+const gridSuplementos = document.querySelector(".swiper-wrapper");
+
+function transformCurrency(value) {
+  const moeda = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(value);
+  return moeda;
+}
+
+let conteudo = "";
+
+suplementos.forEach((item) => {
+  conteudo += `
+  <div class="swiper-slide">
+              <img src="./src/assets/images/bg-img-suplemento2.png" alt="Imagem de uma creatina" />
+              <div class="swiper-slide-content">
+                <h3 class="swiper_title">${item.title}</h3>
+                <p class="swiper_description">${item.subtitle}</p>
+                <div class="de-por">
+                  <p>de: <span class="de">${transformCurrency(item.lastPrice)}</span></p>
+                  <p class="por">por apenas</p>
+                  <p class="preco">${transformCurrency(item.newPrice)}</p>
+                </div>
+                <img src="./src/assets/images/suplementos/${item.imgSrc}" class="suplemento-img" />
+              </div>
+  </div>
+`;
+});
+
+gridSuplementos.innerHTML = conteudo;
+/* ============================================================================= */
